7c880c4a25a6368cee32061eab86c91c6d86b641
Naida Hadzic <naida.popovic1993@gmail.com>
2025-08-23 22:09:32 +0200

    Fix: Replace broken psycopg2 with psycopg2-binary to resolve DB connection error

--- my_project/settings.py
+++ my_project/settings.py
@@ -89,10 +89,7 @@ WSGI_APPLICATION = 'my_project.wsgi.application'
 # }
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / "db.sqlite3",
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
